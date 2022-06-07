9464695fdd98205df7be14d6aaec58f65f7ee530
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2022-06-07 17:52:13 +0000

    aggiunto Magento_Catalogue

---
+++ app/design/frontend/Enrico/Giorgi/web/js/app.js
@@ -0,0 +1,8 @@
+requirejs(['jquery'], function ($) { 
+    
+    let myVar = setInterval(myTimer ,1000);
+    function myTimer() {
+      const d = new Date();
+      document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+    }
+});
