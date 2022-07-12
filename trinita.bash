0ad8c1a715771310d944684c1a6f3c9a044c7e28
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2022-07-12 13:29:25 +0000

    fonts

--- trinita.bash
+++ trinita.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-bin/magento setup:upgrade 
-bin/magento setup:static-content:deploy -f
-bin/magento setup:di:compile
-bin/magento indexer:reindex
-bin/magento cache:clean
-bin/magento cache:flush 
+
+
+
+grunt clean
+grunt exec
+grunt less
+
+grunt watch
