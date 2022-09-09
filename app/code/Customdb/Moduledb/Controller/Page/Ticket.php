08d9ea630ef982ffab176b8b2aa9d3bfdf190145
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2022-09-09 16:53:27 +0000

    collections

--- app/code/Customdb/Moduledb/Controller/Page/Ticket.php
+++ app/code/Customdb/Moduledb/Controller/Page/Ticket.php
@@ -5,28 +5,44 @@ namespace Customdb\Moduledb\Controller\Page;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\App\Action\Context;
-use Customdb\Moduledb\Model\TicketFactory;
-
+//use Customdb\Moduledb\Model\TicketFactory;
+use Customdb\Moduledb\Model\ResourceModel\Ticket\CollectionFactory;
+ 
     class Ticket extends Action
     {
-       
-        protected function __construct(Context $context, TicketFactory $ticketF)
+       protected $resultFactory;
+       protected $tcollectionFactory;
+
+        protected function __construct(
+            Context $context, 
+            CollectionFactory $ticketF,
+            ResultFactory $resultFactory)
         {
 
-            $this->ticketFactory = $ticketF;
+            $this->resultFactory = $resultFactory;
+            $this->collectionFactory = $ticketF;
             parent::__construct($context);
         }
 
     public function execute()
     {
-         /** @var Json $jsonResult */
-        $PageResult=$this->resultFactory->create(ResultFactory::TYPE_PAGE);
-
+        $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
         echo (" ci arrivo");
  
+        $result = $this->collectionFactory->create();//lt sta per lesser than
+        $result->addFieldToFilter('ticketid', array('lt' <= 9));
+        foreach($result as $ticket){
+        print_r($ticket->getData('ticketid'));
+        }       
+        exit;
+        // $collection = $result->getCollection(); //Get Collection of module data
         
         
-        return $PageResult;
+       // $block = $page->getLayout()->getBlock('cmdb_page_ticket');
+       // $block->setData('collection', $collection);
+
+        
+        return $page;
     }
    
 
